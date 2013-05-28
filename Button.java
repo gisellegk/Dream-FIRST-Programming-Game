7e0dc03413006facf3a7d009b580603b53f26aca
Giselle <gisellegk@gmail.com>
2013-05-28 11:21:55 -0300

    Uploaded Button Class

---
+++ Button.java
@@ -0,0 +1,21 @@
+
+public class Button
+{
+    /* int command
+     * 0 = go forward one
+     * 1 = turn left
+     * 2 = turn right
+     * 3 = go forward until... (while)
+     * 4 = wall sensor
+     * 5 = step counter */    
+    private int command;
+    
+    public Button(int c)
+    {
+        command = c;
+    }
+    public int getCommand()
+    {
+        return command;
+    }
+}
